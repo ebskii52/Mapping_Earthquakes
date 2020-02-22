9716892befa51bcbda23eeac98ed3229c7630612
ebskii52 <ebskii52@users.noreply.github.com>
2020-02-22 12:58:37 -0800

    Ipdte

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -9,7 +9,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -18,30 +18,32 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
 	Street: streets,
-	Dark: dark
+	Satellite: satelliteStreets
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [30, 30],
-zoom: 2,
-	layers: [streets]
+	center: [43.7, -79.3],
+zoom: 18,
+	layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/ebskii52/Mapping_Earthquakes/master/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/ebskii52/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
 	console.log(data);
 	// Creating a GeoJSON layer with the retrieved data.
   	L.geoJson(data, {
-	color: "#ffffa1",
-	weight: 2,
+	color: "blue",
+	weight: 1,
 	onEachFeature: function(feature, layer) {
-	  layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline + "</h3>" + "<hr>" + "<h3>" + "Destination: " + feature.properties.dst + "</h3>");
-	}}).addTo(map);
+		layer.bindPopup("<h3>" + "AREA_NAME: " + feature.properties.AREA_NAME + "</h3>" + "<hr>" + "<h3>" + "AREA CODE: " + feature.properties.AREA_S_CD + "</h3>");
+	  }
+
+	}).addTo(map);
 });
