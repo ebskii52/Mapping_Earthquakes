3b283a27d67702bfc6966470ba820e34a07173b3
ebskii52 <ebskii52@users.noreply.github.com>
2020-02-21 17:20:59 -0800

    Update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,10 +4,21 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+L.circleMarker([34.0522, -118.2437], {
+	radius: 300,
+	color: 'black',
+    fillColor: 'light_yellow',
+    fillOpacity: 0.5,
+ }).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker1 = L.marker([34.0522, -118.2437]).addTo(map);
+let marker2= L.marker([38.5816, -121.4944]).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
+	maxZoom: 10,
 	id: 'mapbox.streets',
 	accessToken: API_KEY
 });
