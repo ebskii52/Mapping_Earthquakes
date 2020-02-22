8d03b3a271ecb5ebe7a6da661b62a98c1aa97a70
ebskii52 <ebskii52@users.noreply.github.com>
2020-02-22 12:32:52 -0800

    Update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,3 +1,5 @@
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -29,20 +31,17 @@ zoom: 2,
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-
-
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/ebskii52/Mapping_Earthquakes/master/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/ebskii52/Mapping_Earthquakes/master/torontoRoutes.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  // Grabbing our GeoJSON data.
-	L.geoJson(data, {
-    // We turn each feature into a marker on the map.
-    pointToLayer: function(feature, latlng) {
-	  return L.marker(latlng)
-	  .bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>" + "<hr>" + "<h2>" + "Airport Name: " + feature.properties.name + "</h2>");
+d3.json(torontoData).then(function(data) {
+	console.log(data);
+	// Creating a GeoJSON layer with the retrieved data.
+  	L.geoJson(data, {
+	color: "#ffffa1",
+	weight: 2,
+	onEachFeature: function(feature, layer) {
+	  layer.bindPopup("<h3>" + "Airline: " + feature.properties.airline + "</h3>" + "<hr>" + "<h3>" + "Destination: " + feature.properties.dst + "</h3>");
 	}}).addTo(map);
 });
